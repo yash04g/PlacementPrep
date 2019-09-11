03d39b520d9ccd7dd9da0494965219c64a237c9c
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2019-09-11 21:10:46 +0530

    Add files via upload

--- Graphs/CB Webinar/graphs1.cpp
+++ Graphs/CB Webinar/graphs1.cpp
@@ -25,14 +25,52 @@ public:
 			cout<<endl;
 		}
 	}
+	void bfs(int src,int target){
+		queue<int> q;
+		vector<bool> visited(V,false);
+		vector<int> dist(V,0);
+		vector<int> parent(V,-1); // initially parent of each node is -1, For source it will always be -1
+		q.push(src);
+		visited[src] = true;
+		while(!q.empty()){
+			int node = q.front();
+			q.pop();
+			cout<<node<<" ";
+			for(int neighbour : adjList[node]){
+				if(!visited[neighbour]){
+					visited[neighbour] = true;
+					q.push(neighbour);
+					dist[neighbour] = dist[node]+1;
+					parent[neighbour] = node;
+				}
+			}
+		}
+		cout<<endl;
+		// Print the distance of each node from source node
+		for(int i=0;i<V;i++){
+			cout<<i<<" Node has dist "<<dist[i]<<endl;
+		}
+		// Shortest path of a given node from source
+		cout<<"Shortest distance is "<<dist[target]<<endl;
+		cout<<"Shortest Path is ";
+		int temp = target;
+		while(temp!=-1){
+			cout<<temp<<"<--";
+			temp = parent[temp];
+		}
+		cout<<" -1"<<endl;
+	}
 };
 
 int main(){
-	Graph g(4);
+	Graph g(6);
 	g.addEdge(0,1);
-	g.addEdge(0,2);
-	g.addEdge(0,3);
-	g.addEdge(3,1);
+	g.addEdge(1,2);
+	g.addEdge(0,4);
+	g.addEdge(2,4);
 	g.addEdge(3,2);
+	g.addEdge(3,5);
+	g.addEdge(3,4);
 	g.printAllEdges();
+	g.bfs(0,3);
 }
