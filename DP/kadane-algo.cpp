6cd2daff9befc409489e08e6cf540dcf66a7d793
Sanskar Jethi <sansyrox@gmail.com>
2019-10-01 02:50:14 +0530

    Create kadane-algo.cpp

---
+++ DP/kadane-algo.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int maxSum(int arr[], int n){
+    int cs=0, ms=0;
+
+    for(int i=0; i<n; i++) {
+        if(cs + arr[i]<0) {
+            cs = 0;
+        }
+        cs += arr[i];
+        ms = max(cs,ms);
+    }
+
+    if(ms==0) {
+        ms = *max_element(arr,arr+n);
+    }
+
+    return ms;
+
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<maxSum(arr, n);
+
+    return 0;
+}
