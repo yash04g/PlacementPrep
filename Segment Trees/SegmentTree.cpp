3f9f9c06d26edf788ae246765ba309dc3292d13b
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2019-10-07 18:14:55 +0530

    Add files via upload

---
+++ Segment Trees/SegmentTree.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void buildTree(int *arr,int *tree,int start,int end,int TreeNode){
+	if(start==end){
+		tree[TreeNode] = arr[start];
+		return;
+	}
+	int mid = (start+end)/2;
+	buildTree(arr,tree,start,mid,2*TreeNode);
+	buildTree(arr,tree,mid+1,end,2*TreeNode+1);
+	tree[TreeNode] = tree[2*TreeNode] + tree[2*TreeNode+1];
+}
+void updateTree(int *arr,int *tree,int start,int end,int TreeNode,int idx,int val){
+	if(start==end){
+		arr[idx] = val;
+		tree[TreeNode] = val;
+		return;
+	}
+	int mid = (start+end)/2;
+	if(idx>mid){
+		updateTree(arr,tree,mid+1,end,2*TreeNode+1,idx,val);
+	}else{
+		updateTree(arr,tree,start,mid,2*TreeNode,idx,val);
+	}
+	tree[TreeNode] = tree[2*TreeNode]+tree[2*TreeNode+1];
+}
+int query(int *tree,int start,int end,int TreeNode,int left,int right){
+	// Completely outside
+	if(start>right || end<left){
+		return 0;
+	}
+	//Completely Inside
+	if(start>=left && end<=right){
+		return tree[TreeNode];
+	}
+	// Partially
+	int mid = (start+end)/2;
+	int leftAns = query(tree,start,mid,2*TreeNode,left,right);
+	int rightAns = query(tree,mid+1,end,2*TreeNode+1,left,right);
+	return leftAns+rightAns;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	int *arr = new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int *tree = new int[2*n];
+	buildTree(arr,tree,0,n-1,1);
+	for(int i=1;i<=2*n;i++){
+		cout<<tree[i]<<" ";
+	}
+	cout<<endl;
+	int idx,val;
+	cin>>idx>>val;
+	updateTree(arr,tree,0,n-1,1,idx,val);
+	for(int i=1;i<=2*n;i++){
+		cout<<tree[i]<<" ";
+	}
+	cout<<endl;
+	int left,right;
+	cin>>left>>right;
+	int ans = query(tree,0,n-1,1,left,right);
+	cout<<ans<<endl;
+}
