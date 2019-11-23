681d50fa1fd755ea48823035ccd1dbaf1c76dad4
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2019-11-23 23:56:26 +0530

    Add files via upload

---
+++ main_template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+
+#define ll long long
+#define fo(i,n) for(ll i=0;i<n;++i)
+#define Fo(i,k,n) for(ll i=k;i<n;++i)
+#define vll vector<ll>
+#define vvll vector<vector<ll> >
+#define vpll vector<pair<ll,ll> >
+#define all(v) v.begin(),v.end()
+#define mll map<ll,ll>
+#define mc map<char,ll>  
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define deb(x) cout<<#x<<" "<<x<<endl;
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007
+#define endl "\n"
+#define inf 1e18
+void get_it_done(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+int main(){
+	get_it_done();
+    int t=1;
+    cin >> t;
+    while (t--){
+    	
+    }
+}
