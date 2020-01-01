93abc57cd027ba8366db39ffc4ebad7221c1501a
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-01-01 17:26:06 +0530

    Add files via upload

--- main_template.cpp
+++ main_template.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 #define int long long
-#define fo(i,n) for(int i=0;i<n;++i)
-#define Fo(i,k,n) for(int i=k;i<n;++i)
+#define loop(i,k,n) for(int i=k;i<n;++i)
+#define rloop(i,n,k) for(int i=n;i>=k;i--)
 #define vi vector<int>
 #define vvi vector<vector<int> >
 #define vpii vector<pair<int,int> >
 #define all(v) v.begin(),v.end()
-#define mi map<int,int>
-#define mc map<char,int>  
-#define pll pair<int,int>
+#define mii map<int,int>
+#define mci map<char,int>  
+#define pii pair<int,int>
 #define pdd pair<double,double>
 #define deb(x) cout<<#x<<" "<<x<<endl;
 #define mp make_pair
@@ -26,7 +26,10 @@ void get_it_done(){
 	cin.tie(0);
 	cout.tie(0);
 }
-const int N = 1e5+5;
+void swap(int &a,int &b){
+	a^=b;b^=a;a^=b;
+}
+const int N = 2e5+5;
 int dp[N];
 
 
@@ -35,6 +38,7 @@ int32_t main(){
     int t=1;
     cin >> t;
     while (t--){
-    	
+		    	
+
     }
 }
