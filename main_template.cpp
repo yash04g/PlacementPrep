c57e9fd15f63a026df18ec1a4501aeda1c04f4ca
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-01-29 03:39:03 +0530

    Add files via upload

--- main_template.cpp
+++ main_template.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 #define int long long
+#define ff first
+#define ss second
 #define loop(i,k,n) for(int i=k;i<n;++i)
 #define rloop(i,n,k) for(int i=n;i>=k;i--)
 #define vi vector<int>
 #define vvi vector<vector<int> >
 #define vpii vector<pair<int,int> >
 #define all(v) v.begin(),v.end()
+#define mset(a,i) memset(a,i,sizeof(a))
 #define mii map<int,int>
 #define mci map<char,int>  
 #define pii pair<int,int>
@@ -20,15 +23,18 @@ using namespace std;
 #define mod 1000000007  
 #define endl "\n"
 #define inf 1e18
+#define sp(x,y) fixed<<setprecision(y)<<x
 
 void get_it_done(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 }
-void swap(int &a,int &b){
-	a^=b;b^=a;a^=b;
-}
+int gcd(int a, int b){ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
 const int N = 2e5+5;
 int dp[N];
 
